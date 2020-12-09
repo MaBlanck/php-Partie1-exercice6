c6fbbadb19bf0763cf51ba6ba1272cd234a34afc
Max <maxblanck@outlook.fr>
2020-12-09 09:33:50 +0100

    php Partie 1  Exercice 67

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
+<?php $name = 'Max'; ?>
+
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
+
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Exercice 6 Partie 1</title>
 </head>
+
 <body>
     <h1>Exercice 1 Partie 6</h1>
-
-    <?php
-    $name = 'Max';
-    ?>
-    <?= 'Bonjour ' . $name . 'comment vas tu?'?>
+    <?= 'Bonjour ' . $name . ', comment vas tu?' ?>
 </body>
+
 </html>
